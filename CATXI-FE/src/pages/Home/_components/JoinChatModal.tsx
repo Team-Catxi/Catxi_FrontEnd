6f9ec6eb1b3b351833a9c324efde2408d7116296
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-09-05 01:04:43 +0900

    fix/modal-interaction

--- CATXI-FE/src/pages/Home/_components/JoinChatModal.tsx
+++ CATXI-FE/src/pages/Home/_components/JoinChatModal.tsx
@@ -22,34 +22,47 @@ const JoinChatModalContent = ({ roomId }: Props) => {
         closeModal();
 
         const status = error?.response?.status;
-        const isAlreadyJoined = status === 409;
-        const isStatus = status >= 400 && status < 500;
+        const isForbidden = status === 403;
+        const isClientError = status >= 400 && status < 500;
+        const isServerError = status >= 500;
 
         openModal(
           <InfoModal
-            message={ isAlreadyJoined ? "재로그인 해주세요." : "이미 채팅방에 참여 중입니다."
+            message={
+              isForbidden || isServerError
+                ? "재로그인 해주세요."
+                : isClientError
+                  ? "이미 채팅방에 참여 중입니다."
+                  : "알 수 없는 오류가 발생했습니다."
             }
             onClose={() => {
               closeModal();
-              if (isStatus) {
-                navigate("/");
+              if (isForbidden || isServerError) {
+                navigate("/"); 
               }
             }}
-          />
+          />,
+          { dismissible: false }
         );
       },
     });
   };
 
   return (
-    <div className="z-500">
+    <div className="z-[1000]">
       <h2 className="text-lg font-bold">채팅방 참여</h2>
       <p className="text-md mt-2">이 채팅방에 참여하시겠습니까?</p>
       <div className="flex w-full gap-[1.25rem] mt-4">
-        <button onClick={closeModal} className="flex-1 px-[1.25rem] py-2 text-[#424242] bg-[#F5F5F5] rounded-lg">
+        <button
+          onClick={closeModal}
+          className="flex-1 px-[1.25rem] py-2 text-[#424242] bg-[#F5F5F5] rounded-lg"
+        >
           아니오
         </button>
-        <button onClick={handleJoin} className="flex-1 bg-[#424242] text-[#FAFAFA] px-10 py-2 rounded-lg">
+        <button
+          onClick={handleJoin}
+          className="flex-1 bg-[#424242] text-[#FAFAFA] px-10 py-2 rounded-lg"
+        >
           예
         </button>
       </div>
